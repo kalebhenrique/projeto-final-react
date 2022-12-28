721e4a6c4088327b35e51d6ea32d66746533543b
kalebhenrique <kalebhenriquebr@gmail.com>
2022-12-28 20:07:48 -0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { BrowserRouter } from 'react-router-dom';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 import './global.css'
 
 function App() {
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Navbar/>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
