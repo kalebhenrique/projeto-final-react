8209e53a7a3b9b9701aa2a986f9a4d4299b4d52c
kalebhenrique <kalebhenriquebr@gmail.com>
2022-12-28 22:50:43 -0300

    :truck: rename navbar to header

---
+++ src/components/Header/index.js
@@ -0,0 +1,49 @@
+import { Container } from './styles.js'
+import Logo from '../../assets/Logo.png'
+import Carrinho from '../../assets/Carrinho.png'
+import Menu from '../Menu/index.js'
+
+const Header = () => {
+    return(
+        <Container>
+            <section class='logo'>
+                <table>
+                    <tr>
+                        <th>
+                            <a href='#'>
+                                <img src={Logo} alt='logo site'/>
+                            </a>
+                        </th>
+                        <th>
+                            <a href='#'>
+                                Caneta<br></br><span style={{color: '#1C5D98',}}> azul</span>{' '}<br></br>materiais
+                            </a>
+                        </th>
+                    </tr>
+                </table>
+            </section>
+            <Menu/>
+            <section class='profile_space'>
+                <table>
+                    <tr>
+                        <th>
+                            <a href="">
+                                <img src={Carrinho} alt='carrinho'/>
+                            </a>
+                        </th>
+                        <th>0</th>
+                        <th>
+                            <a href='#'>Entrar</a>
+                            <a href='#'>Criar conta</a>
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>Carrinho</td>
+                    </tr>
+                </table>
+            </section>
+        </Container>
+    )
+}
+
+export default Header
---
+++ src/components/Header/styles.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+    width: 100%;
+    background-color: #00B2FF;
+    height: 7.5rem;
+    display: flex;
+
+    .logo a:link {
+        text-decoration: none;
+        color: #FFC700;
+    }
+    .logo a:hover {
+        text-decoration: underline;
+    }
+
+    .logo {
+        margin: 0 2rem 1rem 1rem;
+        color: #FFC700;
+        align-items: center;
+        font-size: 1.563rem;
+        font-weight: bold;
+        cursor: pointer;
+        text-decoration: none;
+    }
+
+    .logo img {
+        max-width: 6.938rem;
+        height: 7.5rem; 
+        cursor: pointer;
+    }
+
+    .profile_space {
+        margin: 1.3rem 2rem 1rem 1rem;
+        font-size: 15px;
+    }
+
+    .profile_space img{
+        width: 3rem;
+        height: 3rem; 
+    }
+
+    .profile_space a:link {
+        text-decoration: none;
+        color: black;
+    }
+    .profile_space a:hover {
+        text-decoration: underline;
+    }
+`
