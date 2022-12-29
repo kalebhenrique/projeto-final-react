075f52fb1368126b28bf2d6b7cd3eebce9c15789
kalebhenrique <kalebhenriquebr@gmail.com>
2022-12-29 02:15:10 -0300

    :sparkles: add component enter_profile

---
+++ src/components/EnterProfile/index.js
@@ -0,0 +1,20 @@
+import { Container } from './styles.js'
+
+const EnterProfile = () => {
+    return(
+        <Container>
+            <table>
+                <tr>
+                    <th>
+                        <a href='#'>Entrar</a>
+                    </th>
+                    <th>
+                        <a href='#'>Criar conta</a>
+                    </th>
+                </tr>
+            </table>
+        </Container>
+    )
+}
+
+export default EnterProfile
---
+++ src/components/EnterProfile/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    margin: auto 1rem auto 0;
+    font-size: 1rem;
+
+    a {
+        margin: 0 0.8rem
+    }
+
+    a {
+        color: black;
+        text-decoration: none;
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+`
