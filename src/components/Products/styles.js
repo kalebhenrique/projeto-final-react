4ceff3322646a28c5e57923666be920cb25380bf
kalebhenrique <kalebhenriquebr@gmail.com>
2022-12-29 02:14:40 -0300

    :sparkles: add component product

---
+++ src/components/Products/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+    max-width: 1115px;
+    border: solid 2px;
+    margin: 4rem auto;
+    background-color: white;
+
+    p {
+        margin: 8px 2rem;
+        font-weight: bold;
+    }
+
+    div {
+        display : flex;
+        flex-direction: row;
+    }
+`
