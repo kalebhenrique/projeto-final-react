ae09877c631a14fce3e6e5e4ae35748897e19a18
kalebhenrique <kalebhenriquebr@gmail.com>
2022-12-28 22:51:38 -0300

    :sparkles: add component menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,19 @@
+import { Container } from './styles.js'
+import Input from '../Input/index.js'
+
+const Menu = () => {
+    return(
+        <Container>
+            <Input/>
+            <table>
+                <tr>
+                    <th><a href="#">Materiais</a></th>
+                    <th><a href="#">Cadernos</a></th>
+                    <th><a href="#">Mochilas</a></th>
+                </tr>
+            </table>              
+        </Container>
+    )
+}
+
+export default Menu
---
+++ src/components/Menu/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+    margin: 2rem auto 1rem auto;
+    align-items: center;
+    text-decoration: none;
+
+    th {
+        padding: 10px;
+        font-size: 1.3rem;
+    }
+
+    a {
+        color: black;
+        text-decoration: none;
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+`
