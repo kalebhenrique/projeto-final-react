d37ad2bf75fd23d42e951193bdd014541c6ffeba
kalebhenrique <kalebhenriquebr@gmail.com>
2022-12-29 02:19:13 -0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { BrowserRouter } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
+// import ProductsCard from './components/ProductsCard';
+// import Products from './components/Products';
 import './global.css'
+// import Products from './pages/product_page';
 import MyRoutes from './routes';
 
 function App() {
