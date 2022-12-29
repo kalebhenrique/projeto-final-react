67cff684fd6eb277a6db188b5b3a471bfe77cc48
kalebhenrique <kalebhenriquebr@gmail.com>
2022-12-29 02:18:53 -0300

    :sparkles: add page product_page

---
+++ src/pages/product_page/index.js
@@ -0,0 +1,7 @@
+const ProductPage = () => {
+    return(
+        <h1>teste</h1>
+    )
+}
+
+export default ProductPage
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import Homepage from '../pages/homepage/index.js'
-import Products from '../pages/products_page/index.js'
+import Products from '../pages/product_page/index.js'
 
 const MyRoutes = () => {
     return(
             <Routes>
                 <Route path='/' element={<Homepage/>} />
-                <Route path='products' element={<Products/>} />
+                <Route path='/products' element={<Products/>} />
             </Routes>
     )
 }
