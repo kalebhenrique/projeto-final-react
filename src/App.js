3d02ad4c1aaba92a04643b522d9fa400b0b35140
Bernardo <bernardobraga2016@gmail.com>
2022-12-28 22:22:11 -0300

    ➕routes & homepage created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter } from 'react-router-dom';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import './global.css'
+import MyRoutes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar/>
+      <MyRoutes/>
       <Footer/>
     </BrowserRouter>
   );
