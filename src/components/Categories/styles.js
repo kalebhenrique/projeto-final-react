c118ef7d25d990bcc69315acc0d4b4aca6d264df
Bernardo <bernardobraga2016@gmail.com>
2022-12-30 00:05:11 -0300

    Products page created

---
+++ src/components/Categories/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+    max-width: 1115px;
+    border: solid 2px;
+    margin: 4rem auto;
+    background-color: white;
+
+    p {
+        margin: 10px 2rem;
+        font-weight: bold;
+        font-size: 1.3rem;
+    }
+`
---
+++ src/components/Results/styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+export const Container = styled.section`
+
+    p {
+        margin: 8px 2rem;
+        font-weight: bold;
+    }
+
+    div {
+        display : flex;
+        flex-direction: row;
+        margin-bottom: 0rem;
+    }
+
+    h1 {
+        border-radius: 5px;
+        padding: 4px;
+        background-color: #E9E9E9;
+        font-size: 25px;
+        max-width: 10rem;
+        display: flex;
+        flex-direction: row;
+        margin: 0.5rem 1.8rem 0.1rem 1.9rem
+    }
+
+    h2 {
+        background-color: #E9E9E9;
+        padding: 4px;
+        border-radius: 5px;
+        font-size: 18px;
+        max-width: 10rem;
+        display: flex;
+        flex-direction: row;
+        margin: 0.2rem 4.1rem 2rem 3.5rem
+    }
+
+`
