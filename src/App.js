62fe4a21371f0f336b7f09e2d729ad1e341e6729
jessica8melo <jessica8melo@gmail.com>
2022-12-30 19:14:57 -0300

    :sparkles: layout_navbar created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
 import Footer from './components/Footer';
-import Header from './components/Header';
-// import ProductsCard from './components/ProductsCard';
-// import Products from './components/Products';
 import './global.css'
-// import Products from './pages/product_page';
 import MyRoutes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header/>
       <MyRoutes/>
       <Footer/>
     </BrowserRouter>
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,17 @@ import Homepage from '../pages/homepage/index.js'
 import Products from '../pages/product_page/index.js'
 import LoginPage from '../pages/login/index.js'
 import RegisterPage from '../pages/register/index.js'
+import { LayoutNavbar } from '../pages/layout_navbar.js'
 
 const MyRoutes = () => {
     return(
             <Routes>
-                <Route path='/' element={<Homepage/>} />
-                <Route path='/products' element={<Products/>} />
                 <Route path="/login" element={<LoginPage/>} />
                 <Route path='/register' element={<RegisterPage/>} />
+                <Route path='/' element={<LayoutNavbar/>} >
+                    <Route path='/' element={<Homepage/>} />
+                    <Route path='/products' element={<Products/>} />
+                </Route>
             </Routes>
     )
 }
