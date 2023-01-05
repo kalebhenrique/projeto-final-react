572cd15ed909f1e807d4a2610978c75cbf9bab0c
kalebhenrique <kalebhenriquebr@gmail.com>
2023-01-05 20:12:18 -0300

    :fire: remove component Categories

--- src/components/Results/styles.js
+++ src/components/Results/styles.js
@@ -3,4 +3,5 @@ import styled from 'styled-components'
 export const Container = styled.section`
     display : flex;
     flex-direction: row;
+    flex-wrap: wrap;
 `
--- src/pages/category/index.js
+++ src/pages/category/index.js
@@ -1,10 +1,51 @@
-import Categories from '../../components/Categories/index.js'
+import { useParams } from "react-router-dom"
+import Results from "../../components/Results/index.js"
+import { Container } from "./styles.js"
 
 const CategoryPage = () => {
+    const {id} = useParams()
+    console.log(id)
+    let categoryName = 'Materiais'
+    const products = [
+        {
+            'id': 1,
+            'name': 'Caneta azul',
+            'price_in_cents': 199
+        },
+        {
+            'id': 2,
+            'name': 'Caneta azul',
+            'price_in_cents': 199
+        },
+        {
+            'id': 3,
+            'name': 'Caneta azul',
+            'price_in_cents': 199
+        },
+        {
+            'id': 4,
+            'name': 'Caneta azul',
+            'price_in_cents': 199
+        },
+        {
+            'id': 5,
+            'name': 'Caneta azul',
+            'price_in_cents': 199
+        },
+        {
+            'id': 5,
+            'name': 'Caneta azul',
+            'price_in_cents': 199
+        }
+    ]
+
     return(
-        <>
-            <Categories/>
-        </>
+        <Container>
+            <p>Resultados para: {categoryName}</p>
+            <div>
+                <Results products={products}/>
+            </div>         
+        </Container>
     )
 }
 
