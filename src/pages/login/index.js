065d47d8cc06d9d572b11edff5ffa4b88e344afa
jessica8melo <jessica8melo@gmail.com>
2023-01-06 15:25:31 -0300

    useContext added to login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/logo.png'
 import { FormBox } from '../../components/FormBox';
+import { useUserContext } from '../../contexts/useUserContext';
 
 const LoginPage = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
+    const { login, user } = useUserContext();
+    
     return(
         <FormBox>
             <Link to="/"><img src={Logo}/></Link>
@@ -16,7 +18,7 @@ const LoginPage = () => {
                 <input onChangeFunction={setEmail} />
                 <p>Senha</p>
                 <input onChangeFunction={setPassword} type='password' />
-                <button type='button'>Entrar</button>
+                <button onClick={() => login(email, password)}>Entrar</button>
             </div>
         </FormBox>
     )
